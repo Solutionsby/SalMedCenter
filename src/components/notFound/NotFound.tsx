2c0f1e58af4d79fbb8ff509b7d448c9654050e43
Pawel Goc <pawel@Mac-mini-Pawel.local>
2024-02-04 18:21:46 +0100

    page not Found

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,9 +1,24 @@
 import './notFound.scss'
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
 
 
 export const NotFound: React.FC = ()=>{
+    const { t } = useTranslation("translation");
 return(
-    <h2>Test</h2>
+   <div className="page-not-found">
+    <Link className='logo-return' to="/"><div className="page-not-found-logo"></div></Link>.
+    <div className="page-not-found-text">
+    <h1>{t("pageNotFound.title")}</h1>
+    <p>{t("pageNotFound.paragrafOne")}</p>
+    <p>
+        {t("pageNotFound.return")} 
+    </p>
+    <Link to="/">{t("pageNotFound.mainPage")}</Link>
+    
+    </div>
+   </div>
 )
 
 }
