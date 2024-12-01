d5e5fdc2a8bb3ea078eaf4ec360de40fffbb1fad
Pawel Goc <pawel.goc@solutionsby.pl>
2024-12-01 20:08:54 +0100

    update dependecies and change text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { PriceList } from "./components/ priceList/PriceList";
 import { Contact } from "./components/ contact/Contact";
 import { PrivacyPolicyText } from "./components/policy/privacy-policy-text/PrivacyPolicyText";
 import { ServicesPages } from "./components/servicesPage/servicesPages/ServicesPages";
-import {NotFound} from "./components/notFound/NotFound"
+import { NotFound } from "./components/notFound/NotFound";
 import { serviceContent } from "./components/db/services.json";
 import { useEffect, useState } from "react";
 import { initGA, logPageView } from "./components/googleAnalitik/gtag";
@@ -15,129 +15,112 @@ import ScrollToTopEffect from "./assets/ScrollToTop";
 import { Indications } from "./components/treatment/indicationsForTreatment/IndicationsForTreatment";
 import { DisasesPage } from "./components/treatment/indicationsForTreatment/disasesPages/DisasesPage";
 import { Contraindications } from "./components/treatment/contraindications/Contraindications";
-import "./fontAwsome/ImportsFontAwsome"
+import "./fontAwsome/ImportsFontAwsome";
 import "./App.css";
 
 function App() {
-  const [privacAccepted, setPrivacyAccepted] = useState<boolean | null>(null);
-  useEffect(() => {
-    if (privacAccepted) {
-      logPageView(window.location.pathname);
-    }
-    checkPrivacyAccepted();
-  }, [privacAccepted, window.location.pathname]);
+	const [privacAccepted, setPrivacyAccepted] = useState<boolean | null>(null);
+	useEffect(() => {
+		if (privacAccepted) {
+			logPageView(window.location.pathname);
+		}
+		checkPrivacyAccepted();
+	}, [privacAccepted, window.location.pathname]);
 
-  const handlePrivacy = (decision: boolean) => {
-    setPrivacyAccepted(decision);
-    if (decision === true) {
-      const expirationDate = new Date();
-      expirationDate.setMonth(expirationDate.getMonth() + 2);
-      document.cookie = `privacyAccepted=true; expires=${expirationDate.toUTCString()}; path=/`;
-      initGA();
-    }
-  };
-  const checkPrivacyAccepted = () => {
-    const cookies = document.cookie.split(";");
-    for (const cookie of cookies) {
-      const [name, value] = cookie.split("=");
-      if (name.trim() === "privacyAccepted") {
-        setPrivacyAccepted(value === "true");
-        initGA();
-        break;
-      }
-    }
-  };
-  return (
-    <Router>
-      <div className="app-wrapper">
-        <MainNavigation/>
-        <ScrollToTopEffect />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="priceList" element={<PriceList />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="privacy-policy" element={<PrivacyPolicyText />} />
-          
-          <Route
-            path="pedriatic-indection"
-            element={
-              <ServicesPages
-                content={serviceContent[0]}
-                isFirstService={true}
-              />
-            }
-          />
-          <Route
+	const handlePrivacy = (decision: boolean) => {
+		setPrivacyAccepted(decision);
+		if (decision === true) {
+			const expirationDate = new Date();
+			expirationDate.setMonth(expirationDate.getMonth() + 2);
+			document.cookie = `privacyAccepted=true; expires=${expirationDate.toUTCString()}; path=/`;
+			initGA();
+		}
+	};
+	const checkPrivacyAccepted = () => {
+		const cookies = document.cookie.split(";");
+		for (const cookie of cookies) {
+			const [name, value] = cookie.split("=");
+			if (name.trim() === "privacyAccepted") {
+				setPrivacyAccepted(value === "true");
+				initGA();
+				break;
+			}
+		}
+	};
+	return (
+		<Router>
+			<div className="app-wrapper">
+				<MainNavigation />
+				<ScrollToTopEffect />
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="priceList" element={<PriceList />} />
+					<Route path="contact" element={<Contact />} />
+					<Route path="privacy-policy" element={<PrivacyPolicyText />} />
+
+					<Route
+						path="first-consultation"
+						element={
+							<ServicesPages
+								content={serviceContent[0]}
+								isFirstService={true}
+							/>
+						}
+					/>
+					<Route
+						path="follow-up-appointment"
+						element={<ServicesPages content={serviceContent[1]} />}
+					/>
+					{/* <Route
             path="pedriatic-healthy"
             element={
               <ServicesPages
                 content={serviceContent[1]}
               />
             }
-          />
-          <Route
+          /> */}
+					{/* <Route
             path="internist-indection"
             element={
               <ServicesPages
                 content={serviceContent[2]}
               />
             }
-          />
-          <Route
-            path="internist-healthy"
-            element={
-              <ServicesPages
-                content={serviceContent[3]}
-              />
-            }
-          />
-          <Route
+          /> */}
+					{/* <Route
             path="internist"
             element={
               <ServicesPages
                 content={serviceContent[4]}
               />
             }
-          />
-          <Route 
-          path="neoplasm" element={<DisasesPage id={0}/>} />
-          <Route 
-          path="degenerative" element={<DisasesPage id={1}/>} />
-          <Route 
-          path="Neurodegenerative" element={<DisasesPage id={2}/>} />
-          <Route 
-          path="SM" element={<DisasesPage id={3}/>} />
-          <Route 
-          path="Epilepsy" element={<DisasesPage id={4}/>} />
-          <Route 
-          path="Glaucoma" element={<DisasesPage id={5}/>} />
-          <Route 
-          path="Endometriosis" element={<DisasesPage id={6}/>} />
-          <Route 
-          path="Depression" element={<DisasesPage id={7}/>} />
-          <Route 
-          path="Insomnia" element={<DisasesPage id={8}/>} />
-          <Route 
-          path="PTSD" element={<DisasesPage id={9}/>} />
-          <Route 
-          path="Dementia" element={<DisasesPage id={10}/>} />
-          <Route 
-          path="IBS" element={<DisasesPage id={11}/>} />
+          /> */}
+					<Route path="neoplasm" element={<DisasesPage id={0} />} />
+					<Route path="degenerative" element={<DisasesPage id={1} />} />
+					<Route path="Neurodegenerative" element={<DisasesPage id={2} />} />
+					<Route path="SM" element={<DisasesPage id={3} />} />
+					<Route path="Epilepsy" element={<DisasesPage id={4} />} />
+					<Route path="Glaucoma" element={<DisasesPage id={5} />} />
+					<Route path="Endometriosis" element={<DisasesPage id={6} />} />
+					<Route path="Depression" element={<DisasesPage id={7} />} />
+					<Route path="Insomnia" element={<DisasesPage id={8} />} />
+					<Route path="PTSD" element={<DisasesPage id={9} />} />
+					<Route path="Dementia" element={<DisasesPage id={10} />} />
+					<Route path="IBS" element={<DisasesPage id={11} />} />
 
-          <Route path="indications" element={<Indications />} />
-          <Route path="contraindications" element={<Contraindications />} />
+					<Route path="indications" element={<Indications />} />
+					<Route path="contraindications" element={<Contraindications />} />
 
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <PrivacyPolicy
-          privacAccepted={privacAccepted}
-          handlePrivacy={handlePrivacy}
-        />
-        <Footer />
-      </div>
-    </Router>
-  );
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+				<PrivacyPolicy
+					privacAccepted={privacAccepted}
+					handlePrivacy={handlePrivacy}
+				/>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/servicesPage/servicesPages/ServicesPages.tsx
+++ src/components/servicesPage/servicesPages/ServicesPages.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Header } from "../../header/Header";
-import {services} from '../../db/services.json'
+import { services } from "../../db/services.json";
 import { DownloadForm } from "../../downloadForm/DownloadForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
@@ -10,58 +10,68 @@ import { useLanguage } from "../../../context/Context";
 import { Registration } from "../../registration/Registration";
 
 interface priceObject {
-  content: {
-    id:number;
-    price:number;
-    visitHeader: string;
-    headerImg: string;
-  };
-  isFirstService?:boolean
+	content: {
+		id: number;
+		price: number;
+		visitHeader: string;
+		headerImg: string;
+	};
+	isFirstService?: boolean;
 }
 
-export const ServicesPages: React.FC<priceObject> = ({ content, isFirstService,}) => {
-  const { t } = useTranslation("serviceSection");
-  const filtredList = services.filter((_, index)=> index !== content.id)
-  const { currentLanguage} = useLanguage();
-  console.log(currentLanguage)
-  return (
-    <div className="services-pages-wrapper">
-      <Header image={content.headerImg} className="header-service-page">
-        <div className="header-blend">
-          <Registration />
-          <h1>{t(content.visitHeader)}</h1>
-        </div>
-      </Header>
+export const ServicesPages: React.FC<priceObject> = ({
+	content,
+	isFirstService,
+}) => {
+	const { t } = useTranslation("serviceSection");
+	const filtredList = services.filter((_, index) => index !== content.id);
+	const { currentLanguage } = useLanguage();
 
-      <div className="services-pages-descripon-wrapper">
-        <div className="services-descripton">{t(`servicesContent.${content.id}.content`)}</div>
-        <div className="services-price-list">
-        <div className="price-line"></div>
-            <h4>{t(`servicesContent.${content.id}.priceText`)}</h4>
-            <h3>{content.price} zł</h3>
-          <div className="price-line"></div>
+	return (
+		<div className="services-pages-wrapper">
+			<Header image={content.headerImg} className="header-service-page">
+				<div className="header-blend">
+					<Registration />
+					<h1>{t(content.visitHeader)}</h1>
+				</div>
+			</Header>
 
-        </div>
-        {(isFirstService && currentLanguage != "en")&& <div className="first-time-visit-wrapper">
-          <p className="description">{t("servicesContent.0.descripton")}</p>
-          <DownloadForm />        
-          </div>}
-    <div className="service-component service">{filtredList.map(({id,fontAwsomeIcon,descriptionKey,linkPath})=>(
-      <div key={id} className="services-component-wrapper service-in-service">
-      <Link to={linkPath} className="services-links">
-        <div className="service-icon">
-          <FontAwesomeIcon icon={fontAwsomeIcon as IconProp} />
-        </div>
-        <div className="service-texts">
-          <h2 className="service-description">{t(descriptionKey)}</h2>
-          <p>{t("services.more")}</p>
-        </div>
-      </Link>
-    </div>
-
-    ))}</div>
-      </div>
-
-    </div>
-  );
+			<div className="services-pages-descripon-wrapper">
+				<div className="services-descripton">
+					{t(`servicesContent.${content.id}.content`)}
+				</div>
+				<div className="services-price-list">
+					<div className="price-line"></div>
+					<h4>{t(`servicesContent.${content.id}.priceText`)}</h4>
+					<h3>{content.price} zł</h3>
+					<div className="price-line"></div>
+				</div>
+				{isFirstService && currentLanguage != "en" && (
+					<div className="first-time-visit-wrapper">
+						<p className="description">{t("servicesContent.0.descripton")}</p>
+						<DownloadForm />
+					</div>
+				)}
+				<div className="service-component service">
+					{filtredList.map(
+						({ id, fontAwsomeIcon, descriptionKey, linkPath }) => (
+							<div
+								key={id}
+								className="services-component-wrapper service-in-service">
+								<Link to={linkPath} className="services-links">
+									<div className="service-icon">
+										<FontAwesomeIcon icon={fontAwsomeIcon as IconProp} />
+									</div>
+									<div className="service-texts">
+										<h2 className="service-description">{t(descriptionKey)}</h2>
+										<p>{t("services.more")}</p>
+									</div>
+								</Link>
+							</div>
+						)
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
